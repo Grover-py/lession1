e476eb8886816e4e0b964db86b4f792ca4881e0b
Grover_py <Nebu23z@yandex.ru>
2021-01-18 22:13:28 +0300

    finish him

--- main.py
+++
@@ -1,3 +0,0 @@
-my_list = [7, 5, 3, 3, 2]
-my_list.insert(100,8)
-print(my_list)
--- task_04.py
+++ task_04.py
@@ -1,7 +1,5 @@
 my_str = input('Введите данные через пробел: ')
-my_list = my_str.split()
-
 n = 0
-for i in my_list:
-    print(f'{n+1}: {my_list[n][0:10]}')
+for i in my_str.split():
+    print(f'{n+1}: {my_str.split()[n][0:10]}')
     n += 1
